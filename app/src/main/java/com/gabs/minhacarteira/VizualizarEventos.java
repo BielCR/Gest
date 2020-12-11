b50a4cd8def431d68fd7acdf1422e86b8d259e83
BielCR <gabrielcr18@outlook.com>
2020-12-10 23:36:51 -0300

    Implementando novas classes e integrando o xml

---
+++ app/src/main/java/com/gabs/minhacarteira/VizualizarEventos.java
@@ -0,0 +1,14 @@
+package com.gabs.minhacarteira;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class    VizualizarEventos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vizualizar_eventos);
+    }
+}
